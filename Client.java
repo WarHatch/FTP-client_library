495b1857e58b0544a7bc17153eabcbb0cb5463dd
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2018-04-04 13:39:47 +0300

    SendToServer and GetServerResponse methods added + stable anonymous login

--- Client.java
+++ Client.java
@@ -1,107 +1,98 @@
 import java.io.*;
+import java.net.Socket;
 
+//TODO remove apache libraries
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.ftp.FTPCmd;
 
-public class Client {
-    private static void showServerReply(FTPClient ftpClient) {
-        String[] replies = ftpClient.getReplyStrings();
-        if (replies != null && replies.length > 0) {
-            for (String aReply : replies) {
-                System.out.println("SERVER: " + aReply);
+public class Client
+{
+    private static String GetServerResponse(BufferedReader server_input)
+    {
+        String server_response = null;
+        try {
+            server_response = server_input.readLine();
+            if (server_response == null) {
+                System.err.println("Warning: No response gotten");
             }
         }
+        catch (IOException e)
+        {
+            System.err.println("Error #9998: Server response exception: "+ e.getMessage());
+        }
+        System.out.println("# " + server_response);
+
+        return server_response;
+    }
+
+    private static String SendToServer(PrintWriter server_output, BufferedReader server_input, String message)
+    {
+        server_output.println(message);
+
+        return GetServerResponse(server_input);
     }
 
     public static void main(String[] args) {
-        String server = "localhost";
-        int port = 21;
-        String user = "anonymous";
-        String pass = "";
-
-        String downloadPath = "D:/Downloads/FromFTP/";
-
-        FTPClient ftpClient = new FTPClient();
         try {
-
-            ftpClient.connect(server, port);
-            showServerReply(ftpClient);
-            ftpClient.login(user, pass);
-            showServerReply(ftpClient);
-            ftpClient.enterLocalPassiveMode();
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-
-            // APPROACH #1: using retrieveFile(String, OutputStream)
-            String remoteFile1 = "/textFile.txt";
-            File downloadFile1 = new File(downloadPath + "downloadedFile.txt");
-            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
-            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
-            showServerReply(ftpClient);
-
-            outputStream1.close();
-
-            if (success) {
-                System.out.println("File #1 has been downloaded successfully.");
-            }
-            else {
-                System.out.println("File has not been downloaded successfully. Returned: "+ success);
-            }
-
-            // APPROACH #2: using InputStream retrieveFileStream(String)
-            String remoteFile2 = "/sortgif.gif";
-            File downloadFile2 = new File(downloadPath + "gotGif.gif");
-            OutputStream outputStream2 = new BufferedOutputStream(new FileOutputStream(downloadFile2));
-            InputStream inputStream = ftpClient.retrieveFileStream(remoteFile2);
-            byte[] bytesArray = new byte[4096];
-            int bytesRead = -1;
-            while ((bytesRead = inputStream.read(bytesArray)) != -1) {
-                outputStream2.write(bytesArray, 0, bytesRead);
-            }
-
-            showServerReply(ftpClient); //INSPECTING
-            success = ftpClient.completePendingCommand();
-            showServerReply(ftpClient); //INSPECTING
-            if (success) {
-                System.out.println("File #2 has been downloaded successfully.");
-            }
-            outputStream2.close();
-            inputStream.close();
-
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (ftpClient.isConnected()) {
-                    ftpClient.logout();
-                    ftpClient.disconnect();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        //##############################################################
-
-        try {
-            String inputBuffer = null;
-            boolean exitFlag = false;
-
             BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.println("*** Welcome to FTPclient ***");
-            while (!exitFlag) {
-                System.out.print("> ");
-                inputBuffer = consoleReader.readLine();
 
-                if (inputBuffer.matches("^exit") || inputBuffer.matches("^quit"))
-                    exitFlag = true;
-                else
-                {
-                    System.out.println("# Attempting anonymous login");
-                    
-                }
+            String downloadPath = "D:/Downloads/FromFTP/";
+//          if (inputBuffer.matches("^exit") || inputBuffer.matches("^quit"))
+//              exitFlag = true;
+//          else
+//          {
+            System.out.println("- Attempting anonymous login");
+
+            //TODO add params input
+            String server = "localhost";
+            int port = 21;
+            String user = "anonymous";
+            String pass = "";
+            System.out.println("localhost:21\nuser: 'anonymous'\npass:''");
+
+            Socket socket = new Socket(server, port);
+            PrintWriter server_out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader server_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String serverPrefix = "# ";
+            String connection_response = server_in.readLine();
+            System.out.println(serverPrefix + connection_response);
+
+//TESTING
+//            FTPClient ftpClient = new FTPClient();
+//            ftpClient.connect(server, port); System.out.println(ftpClient.getReplyString());
+//            ftpClient.sendCommand(FTPCmd.USER, user); System.out.println(ftpClient.getReplyString());
+
+            SendToServer(server_out, server_in, "USER anonymous");
+            SendToServer(server_out, server_in, "PASS satelistas@gmail.com");
+
+            //Gets all server responses
+            String server_response = server_in.readLine();
+            while (!(server_response == null || server_response.equals("")))
+            {
+                System.out.println(server_response);
+                server_response = server_in.readLine();
             }
+
+
+//          String inputBuffer = null;
+//          String outputBuffer = null;
+//          boolean exitFlag = false;
+//          while (!exitFlag) {
+//              System.out.print("> ");
+//              inputBuffer = consoleReader.readLine();
+//
+//              inputBuffer = null;
+//              outputBuffer = null;
+//          } //End of while for program
+
+            server_in.close();
+            server_out.close();
+            socket.close();
         }
         catch (Exception e){
             System.err.println("Error #9999: Unexpected exception: "+
