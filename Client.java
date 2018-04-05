4204abf5e53ceee6111b9e9ba6d8d8e91da3cd70
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2018-04-05 16:44:21 +0300

    Second connection (data) to ftp server

--- Client.java
+++ Client.java
@@ -1,28 +1,27 @@
 import java.io.*;
 import java.net.Socket;
 
-//TODO remove apache libraries
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
-import org.apache.commons.net.ftp.FTPCmd;
-
 public class Client
 {
+    static int serverConnectPort = 21; //The FTP port
+    static int serverDataPort = -1;
+
     private static String GetServerResponse(BufferedReader server_input)
     {
         String server_response = null;
         try {
-            server_response = server_input.readLine();
-            if (server_response == null) {
-                System.err.println("Warning: No response gotten");
+            while (!server_input.ready()) {
+                //just waits
+            }
+            while (server_input.ready()) {
+                server_response = server_input.readLine();
+                System.out.println("# " + server_response);
             }
         }
         catch (IOException e)
         {
             System.err.println("Error #9998: Server response exception: "+ e.getMessage());
         }
-        System.out.println("# " + server_response);
 
         return server_response;
     }
@@ -31,7 +30,36 @@ public class Client
     {
         server_output.println(message);
 
-        return GetServerResponse(server_input);
+        String serverResponse = GetServerResponse(server_input);
+
+        if (message.equals("PASV")) {
+            if (serverResponse.matches("227 Entering Passive Mode \\(\\d+\\,\\d+\\,\\d+\\,\\d+\\,\\d+\\,\\d+\\)\\.") )
+            {
+                String ip_port = serverResponse.split("[\\(\\)]")[1];
+                String[] numbers = ip_port.split("\\,");
+
+                String ip = "";
+                for (int i = 0; i<3 ; i++)
+                    ip += numbers[i]+".";
+                ip += numbers[3];
+                //System.out.print(ip);
+
+                int port = (Integer.parseInt(numbers[4]) * 256 + Integer.parseInt(numbers[5]));
+                serverDataPort = port;
+                System.out.println("- port: "+port);
+            }
+        }
+
+        return serverResponse;
+    }
+
+    private static String SendToServer(PrintWriter server_output, BufferedReader server_input, String message, boolean echo)
+    {
+        if (echo)
+            System.out.println("- " + message);
+
+        String serverResponse = SendToServer(server_output, server_input, message);
+        return serverResponse;
     }
 
     public static void main(String[] args) {
@@ -49,54 +77,65 @@ public class Client
 
             //TODO add params input
             String server = "localhost";
-            int port = 21;
             String user = "anonymous";
-            String pass = "";
-            System.out.println("localhost:21\nuser: 'anonymous'\npass:''");
+            String pass = "gelinislakavimas@one.lt";
 
-            Socket socket = new Socket(server, port);
-            PrintWriter server_out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader server_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("localhost:21\nuser: 'anonymous'\npass: [email adress]");
+
+            Socket connectSocket = new Socket(server, serverConnectPort);
+            PrintWriter server_out = new PrintWriter(connectSocket.getOutputStream(), true);
+            BufferedReader server_in = new BufferedReader(new InputStreamReader(connectSocket.getInputStream()));
 
             String serverPrefix = "# ";
             String connection_response = server_in.readLine();
             System.out.println(serverPrefix + connection_response);
 
-//TESTING
-//            FTPClient ftpClient = new FTPClient();
-//            ftpClient.connect(server, port); System.out.println(ftpClient.getReplyString());
-//            ftpClient.sendCommand(FTPCmd.USER, user); System.out.println(ftpClient.getReplyString());
-
             SendToServer(server_out, server_in, "USER anonymous");
             SendToServer(server_out, server_in, "PASS satelistas@gmail.com");
 
-            //Gets all server responses
-            String server_response = server_in.readLine();
-            while (!(server_response == null || server_response.equals("")))
-            {
-                System.out.println(server_response);
-                server_response = server_in.readLine();
-            }
+            //UNDONE
+            SendToServer(server_out, server_in, "PASV", true);
+            Socket dataSocket = new Socket(server, serverDataPort);
+            PrintWriter data_out = new PrintWriter(dataSocket.getOutputStream(), true);
+            BufferedReader data_in = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
 
+            //Might get a connection confirmation
+            while (data_in.ready())
+                GetServerResponse(data_in);
 
-//          String inputBuffer = null;
-//          String outputBuffer = null;
-//          boolean exitFlag = false;
-//          while (!exitFlag) {
-//              System.out.print("> ");
-//              inputBuffer = consoleReader.readLine();
-//
+            SendToServer(server_out, server_in, "LIST", true);
+            while (server_in.ready())
+                GetServerResponse(server_in);
+
+            //----------------- User interaction block
+            String inputBuffer = null;
+            boolean exitFlag = false;
+            while (!exitFlag) {
+                while (server_in.ready())
+                    GetServerResponse(server_in);
+                while (data_in.ready())
+                    GetServerResponse(data_in);
+
+                System.out.print(">");
+                inputBuffer = consoleReader.readLine();
+
+                if (!inputBuffer.equals(""))
+                    SendToServer(server_out, server_in, inputBuffer);
+
 //              inputBuffer = null;
 //              outputBuffer = null;
-//          } //End of while for program
-
+            } //End of any User Interaction
             server_in.close();
             server_out.close();
-            socket.close();
+            connectSocket.close();
+            dataSocket.close();
+            data_out.close();
+            data_in.close();
         }
         catch (Exception e){
             System.err.println("Error #9999: Unexpected exception: "+
                     e.getMessage());
         }
+        System.out.println("*** Goodbye! ***");
     } //---- End of main
 }
